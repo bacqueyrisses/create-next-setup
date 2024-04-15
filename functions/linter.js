bee0ccce4345503e5b2eaababe5c123899179d86
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-04-15 20:51:25 +0800

    fix: update rws script

--- functions/linter.js
+++ functions/linter.js
@@ -112,69 +112,70 @@ npx --no-install commitlint --edit "\$1"`,
 
   fs.writeFileSync(
     ".husky/helpers/rws.js",
-    `const fs = require("fs");
-const path = require("path");
+    `
+  const fs = require("fs");
+  const path = require("path");
 
-const directoriesToIgnore = ["node_modules", ".git", ".next", ".husky"];
-const acceptedFileExtensions = [".js", ".jsx", ".ts", ".tsx"];
-const defaultRootDirectory = "./";
+  const directoriesToIgnore = ["node_modules", ".git", ".next", ".husky"];
+  const acceptedFileExtensions = [".js", ".jsx", ".ts", ".tsx"];
+  const defaultRootDirectory = "./";
 
-function removeWhiteSpaceFromJSX(jsxString) {
-  const regex = /\\\\s+/g; // Escape backslashes
-  return jsxString.replace(regex, " ").trim();
-}
+  function removeWhiteSpaceFromJSX(jsxString) {
+    const regex = /\\\\s+/g; // Escape backslashes
+    return jsxString.replace(regex, " ").trim();
+  }
 
-function shouldIgnoreDirectory(directoryName) {
-  return directoriesToIgnore.includes(directoryName);
-}
+  function shouldIgnoreDirectory(directoryName) {
+    return directoriesToIgnore.includes(directoryName);
+  }
 
-function processDirectory(directoryPath) {
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
+  function processDirectory(directoryPath) {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.error("Error reading directory:", err);
+        return;
+      }
 
-    files.forEach((file) => {
-      const filePath = path.join(directoryPath, file);
+      files.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
 
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          console.error("Error getting file stats:", err);
-          return;
-        }
-
-        if (stats.isDirectory()) {
-          if (!shouldIgnoreDirectory(file)) {
-            processDirectory(filePath);
+        fs.stat(filePath, (err, stats) => {
+          if (err) {
+            console.error("Error getting file stats:", err);
+            return;
           }
-        } else if (acceptedFileExtensions.includes(path.extname(file))) {
-          fs.readFile(filePath, "utf8", (err, data) => {
-            if (err) {
-              console.error("Error reading file:", err);
-              return;
-            }
 
-            const modifiedContent = data.replace(
-              /className={"(.*?)"}|className="(.*?)"|className={\\\\s*"(.*?)"\\\\s*}|className={\`(.*?)\`}/gs,
-              (
-                match,
-                classNameWithBraces,
-                classNameWithoutBraces,
-                classNameWithBrackets,
-                classNameWithBackticks,
-              ) => {
-                let className =
-                  classNameWithBraces ||
-                  classNameWithoutBraces ||
-                  classNameWithBrackets ||
-                  classNameWithBackticks;
-                if (classNameWithBackticks) {
-                  // Check if there is a variable inside the backticks
-                  if (/\\\${.*?}/.test(classNameWithBackticks)) { // Escape backslash
-                    // Handle class name with backticks containing a variable
-                    // You can replace this logic with your specific handling
-                    return \`className={\`$\{removeWhiteSpaceFromJSX(classNameWithBackticks).trim()}
+          if (stats.isDirectory()) {
+            if (!shouldIgnoreDirectory(file)) {
+              processDirectory(filePath);
+            }
+          } else if (acceptedFileExtensions.includes(path.extname(file))) {
+            fs.readFile(filePath, "utf8", (err, data) => {
+              if (err) {
+                console.error("Error reading file:", err);
+                return;
+              }
+
+              const modifiedContent = data.replace(
+                /className={"(.*?)"}|className="(.*?)"|className={\\\\s*"(.*?)"\\\\s*}|className={\`(.*?)\`}/gs,
+                (
+                  match,
+                  classNameWithBraces,
+                  classNameWithoutBraces,
+                  classNameWithBrackets,
+                  classNameWithBackticks,
+                ) => {
+                  let className =
+                    classNameWithBraces ||
+                    classNameWithoutBraces ||
+                    classNameWithBrackets ||
+                    classNameWithBackticks;
+                  if (classNameWithBackticks) {
+                    // Check if there is a variable inside the backticks
+                    if (/\\\${.*?}/.test(classNameWithBackticks)) { // Escape backslash
+                      // Handle class name with backticks containing a variable
+                      // You can replace this logic with your specific handling
+                      return \`className={\`$\{removeWhiteSpaceFromJSX(classNameWithBackticks).trim()}
 \`}\`;
                   } else {
                     // Handle class name with backticks without a variable
@@ -205,7 +206,7 @@ const rootDirectory = process.argv[2] || defaultRootDirectory;
 processDirectory(rootDirectory);
 console.log("🧹...");
 `
-  );
+  ,  "utf8");
 
 
   // Create .lintstagedrc.js
